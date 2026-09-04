08424370114094f492293a4196b20567f551b2c9
akshat-bit420 <as.93688akshat@gmail.com>
2026-09-04 16:23:07 +0530

    learnt functions can return multiple vals

--- learn_go/functions/custom_func.go
+++ learn_go/functions/custom_func.go
@@ -1,17 +1,25 @@
 package main
 import "fmt"
 
-func add(x int, y int) int {
-	// func is the keyword used to make a function in go,
-	// more importantly the declaration of variable type is
-	// very different in Go, in comparison with it's parent
-	// language "C", c wants to know the variable(parameter in this case) type before 
-	// it's being made where as in Go, it's opposite.
-	//  and it's mandatory to explicitly mentions the return type.
-	return x + y
+// func add(x int, y int) int {
+// 	 func is the keyword used to make a function in go,
+// 	 more importantly the declaration of variable type is
+// 	 very different in Go, in comparison with it's parent
+// 	 language "C", c wants to know the variable(parameter in this case) type before 
+// 	 it's being made where as in Go, it's opposite.
+// 	 and it's mandatory to explicitly mentions the return type.
+// 	return x + y
+// }
+func swap(x,y string) (string, string) {
+	return y,x
+// Go can also return multiple values from inside, it takes 2 parameter.
+// as both the parameters are same we have merged it via single string for declaration
+// just takes x and y and return y inplace of x and x in place of y.
 }
-
 func main() {	
-	fmt.Println(add(12,13))
-	// called the function 
+	a , b := swap("Hello", "world")
+	// the  " := " is used when we don't have to declare the variable type,
+	// as Go does it automatically, just like python.
+
+	fmt.Println(a, b)
 }
