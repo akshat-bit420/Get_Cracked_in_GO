47962437573b61d1e4acadffd60d8d0e1a45eea6
akshat-bit420 <as.93688akshat@gmail.com>
2026-09-04 15:36:19 +0530

    new commit

--- learn_go/random.go
+++ learn_go/random.go
@@ -2,7 +2,12 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	// here rand represents that random package will be used, so we have to provide the path.
+	// the package name has to be in lower case.
+	// the package name should be short cause it easy to use.\
+	// package name should match the folder name(helps to write good go.)
 )
 func main() {
 	fmt.Println("My fav No. is", rand.Intn(10))
+	// works same as python random function, gives random number between 0-9.
 }
