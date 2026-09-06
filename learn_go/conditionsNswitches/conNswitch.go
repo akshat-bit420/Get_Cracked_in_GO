3c0baee7a2e72831288e79d2c9ffe2510e8e4f78
akshat-bit420 <as.93688akshat@gmail.com>
2026-09-06 16:17:36 +0530

    learnt about swtiches and behaviour of print

--- learn_go/conditionsNswitches/conNswitch.go
+++ learn_go/conditionsNswitches/conNswitch.go
@@ -1,25 +1,37 @@
 package main
 import (
 	"fmt"
-	"math"
+	"runtime"
 )
-func pow(x, n, lim float64) float64 {
-	if v := math.Pow(x, n); v < lim{
-		// in this condition we can see we made a variable that has a value,
-		// and in the same line we also had our condition separated by " ; ",
-		// v is only initialized in the "if" block so it can't accessed outside the func.
-		return v
-	} else {
-		fmt.Printf("%g >= %g\n",v, lim)
-	}
-	// won't be able to use v here though, only in a true branch of if-else conditions.
-	
-	return lim
-}
+// func pow(x, n, lim float64) float64 {
+// 	if v := math.Pow(x, n); v < lim{
+// 		// in this condition we can see we made a variable that has a value,
+// 		// and in the same line we also had our condition separated by " ; ",
+// 		// v is only initialized in the "if" block so it can't accessed outside the func.
+// 		return v
+// 	} else {
+// 		fmt.Printf("%g >= %g\n",v, lim)
+// 	}
+// 	// won't be able to use v here though, only in a true branch of if-else conditions.
+
+// 	return lim
+// }
+
+// func main() {
+// 	fmt.Println(
+// 		pow(3,2,10),
+// 		pow(3,3,10),
+// 	)
+// }
 
 func main() {
-	fmt.Println(
-		pow(3,2,10),
-		pow(3,3,10),
-	)
+	fmt.Print("Go runs on:- ")
+	switch os := runtime.GOOS; os {
+	case "windows":
+		fmt.Printf("%s. But I wanna use arch though",os)
+	case "arch":
+		fmt.Println("I use arch btw.")
+	default:
+		fmt.Printf("%s.\n", os)		
+	}
 }
