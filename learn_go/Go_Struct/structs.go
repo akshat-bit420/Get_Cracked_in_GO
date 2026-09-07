0952ce1a54514916b4340d08e755f9033181f7a2
akshat-bit420 <as.93688akshat@gmail.com>
2026-09-07 12:13:54 +0530

    examples how pointer&structs work together

--- learn_go/Go_Struct/structs.go
+++ learn_go/Go_Struct/structs.go
@@ -5,9 +5,18 @@ type Vertex struct {
 	x int 
 	y int
 }
+// func main() {
+// 	v := Vertex{1,9}
+// 	v.x = 2
+// 	// the ' . ' is used to access the values of a struct. 
+// 	fmt.Println(v.x)
+// }
+/*How pointers and structs will work together*/
 func main() {
-	v := Vertex{1,9}
-	v.x = 2
-	// the ' . ' is used to access the values of a struct. 
-	fmt.Println(v.x)
+	v  := Vertex{7,8}
+	p  := &v
+	p.x = 2e6
+	// 1e notation works as power but only in terms of 10,
+	// 2e6 means 6 times 0 after 2
+	fmt.Println(v)
 }
