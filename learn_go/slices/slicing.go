44c5ea48b69d01d1fa7595ea5d61859429ec6399
akshat-bit420 <as.93688akshat@gmail.com>
2026-09-11 16:42:34 +0530

    learning about capacity and slices

--- learn_go/slices/slicing.go
+++ learn_go/slices/slicing.go
@@ -5,28 +5,28 @@
 package main
 import "fmt"
 
-func main() {
-	q := []int{2,3,5,7,9,11}
-	// it's a slice not an array, array will always have a number in them	
-	fmt.Println(q)
+// func main() {
+// 	q := []int{2,3,5,7,9,11}
+// 	// it's a slice not an array, array will always have a number in them	
+// 	fmt.Println(q)
 
-	r := []bool{true,false,true,true,false,false}
-	fmt.Println(r)
+// 	r := []bool{true,false,true,true,false,false}
+// 	fmt.Println(r)
 
-	s := []struct {
-		i int
-		b bool
-	}{
-		{2,true},
-		{3,false},
-		{5,true},
-		{7,true},
-		{11,false},
-		{13,false},
-	}
-	fmt.Println(s)
+// 	s := []struct {
+// 		i int
+// 		b bool
+// 	}{
+// 		{2,true},
+// 		{3,false},
+// 		{5,true},
+// 		{7,true},
+// 		{11,false},
+// 		{13,false},
+// 	}
+// 	fmt.Println(s)
 
-}
+// }
 
 
 // func main() {
@@ -54,3 +54,28 @@ func main() {
 // 	// // 5 is at index 2. Index 3 (the 7) is the first value left out.
 // 	// fmt.Println(s)
 // }
+
+func main() {
+	num :=[]int{2,3,5,7,11,13}
+
+	num = num[1:4]
+	// here the slice will begin from the second index which is 1 and until it,
+	// reaches to the third index, cause 4 is seen as 4 - 1, not 4 exactly.
+	// which means num = [3,5,7]
+	fmt.Println(num)
+
+	num = num[:2]
+	// here the slices is from index 0 until it reaches index 2. 
+	// so num = [3,5]
+	fmt.Println(num)
+
+	num = num[1:]
+	// here the element after index 1 is taken and after 1.
+	// so num = [5]
+	fmt.Println(num)
+}
+/* like this,
+[3 5 7]
+[3 5]
+[5]
+*/
